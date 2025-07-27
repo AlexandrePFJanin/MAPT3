f07372baee52cd290f9edf3374b82b41a31638fa
Alexandre Janin <alexandre.janin7@gmail.com>
2025-07-27 21:22:08 +0200

    setup update PEP 517
    Updated installation configuration for pip and PEP 517 compliance

--- setup.py
+++ setup.py
@@ -1,24 +1,32 @@
 from setuptools import setup
 
 setup(
-   name='MAPT3',
-   version='0.1.1',
-   author='Alexandre JANIN',
-   author_email='alexandre.janin@protonmail.com',
-   url='https://github.com/AlexandrePFJanin/MAPT3',
-   packages=['MAPT3'],
-   license='LICENSE.md',
-   description='Multi-disciplinary and Automatic Plate Tessellation and Time tracking Toolkit.',
-   long_description=open('README.md').read(),
-   install_requires=[
+    name='MAPT3',
+    version='0.1.2',
+    author='Alexandre JANIN',
+    author_email='alexandre.janin@protonmail.com',
+    url='https://github.com/AlexandrePFJanin/MAPT3',
+    description='Multi-disciplinary and Automatic Plate Tessellation and Time tracking Toolkit.',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    license='Apache License 2.0',
+    packages=['MAPT3'],
+    include_package_data=True,
+    install_requires=[
         'ipython>=8.15.0',
         'numpy>=1.12',
         'matplotlib>=3.0',
-	    'cartopy>=0.18',
-	    'scipy>=1.5.2',
-	    'tqdm>=4.65.0',
-	    'alphashape>=1.3.1',
-	    'shapely>=2.0.2',
-	    'h5py>=3.9.0'
-   ],
+        'cartopy>=0.18',
+        'scipy>=1.5.2',
+        'tqdm>=4.65.0',
+        'alphashape>=1.3.1',
+        'shapely>=2.0.2',
+        'h5py>=3.9.0'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
 )
