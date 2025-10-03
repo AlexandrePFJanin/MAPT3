9423c99426b31bf43f5ecbc2e5873f429f68ed0f
Alexandre Janin <alexandre.janin7@gmail.com>
2025-10-03 15:12:20 -0400

    Correction version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MAPT3',
-    version='0.1.2',
+    version='1.0.0',
     author='Alexandre JANIN',
     author_email='alexandre.janin@protonmail.com',
     url='https://github.com/AlexandrePFJanin/MAPT3',
